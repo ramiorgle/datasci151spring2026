0284ac0fda0087dee0b9874700ea0704803ac669
ramiorgle <rami.orgle@emory.edu>
2026-02-07 15:18:23 -0500

    hw 3 work

---
+++ lectures 1-7 personal notes.jl
@@ -0,0 +1,38 @@
+lecture 5 & 6 notes:
+you can "mutate" or change values in lists but you cannot do so with tuples
+lists use []; tuples use () but you can also just list them out without any encasing
+you can print characters in a string the same way you print values in a list
+you cannot mutate strings (just like tuples)
+using + combines lists, strings, or tuples
+you don't have to fully create variables before combining. you can add them in the variable definition
+if you redefine an object in a list as a new variable, if you change that object in the list by selection, then print the new variable again, it will print the old value because that is how you initially defined it
+1 & () are strings; (1,) & 1, are tuples; [1] & [1,] are lists
+barplot: plt.bar(x = categories, height = list of values you correlated with the categories); plt.ylabel gives y axis label; plt.title; plt.show()
+NumPy arrays store numbers as arrays
+you can add (+) arrays of the same length
+if you add an array to a single number, like 2, it will add 2 to each value in the array
+when adding arrays of the same length, it will add the equivalent positions. for example, it will add the first value of both arrays etc.
+The usual mathematical operations - addition, subtraction, multiplication, division, and exponentiation are standard in Python.
+(2**5) means 2 to the power of 5 (2^5)
+# log(x) computes the logarithm with base "e" (Euler's number)
+    # in high school you might have called this ln(x)
+
+# exp(x) compute Euler's number raised to the power of "x"
+
+# sin(x) computes the sine of x (in radians)
+
+# cos(x) computes the cosine of x (in radians)
+
+# In this example, we're substituting x = 2
+
+Euler's number: 2.718281828459045
+
+lecture 7:
+booleans take on one of two value options (ex: true or false)
+When comparing Numpy arrays, a separate Boolean expression is returned for **each element**
+however when comparing strings, lists and tuples, it compares if the entire thing is the same
+when using the in function, it will look for words in a list of strings not specific letters
+logical operators: not, and, or
+control statements: if, elif, else
+
+
